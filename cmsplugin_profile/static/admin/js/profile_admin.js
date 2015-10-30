9601c4d89caa2d97b9d600d1db7467c955657d89
Adelina Diacu <adelina.diacu@3pillarglobal.com>
2015-10-30 16:22:15 +0200

    LUN-2698: remove draggable option from profile grid on edit mode

--- cmsplugin_profile/static/admin/js/profile_admin.js
+++ cmsplugin_profile/static/admin/js/profile_admin.js
@@ -3,6 +3,7 @@
 
 		$( ".grid-list" ).sortable({
 		  items: "> .inline-related",
+		  cancel: ".edit-mode",
 		  update: function(event, ui) {
 		  	$('.order_field input').each(function(i){$(this).attr('value', i+1)});
 		  }
@@ -10,13 +11,12 @@
 		
 		$(document).on('click', '.profile-item-actions .edit-profile-item', function(e){
 			e.preventDefault();
-			$(this).parent('.profile-item-actions').siblings().addClass('visible');
+			$(this).parent('.profile-item-actions').siblings().addClass('visible').closest('.inline-related').addClass('edit-mode');
 			$(this).closest('.grid-list').siblings('.overlay').addClass('visible');
 		});
 		$(document).on('click', '.grid-list .close-profile', function(e){
 			e.preventDefault();
-
-			$(this).closest('.visible').removeClass('visible');
+			$(this).closest('.visible').removeClass('visible').closest('.inline-related').removeClass('edit-mode');
 			$(this).closest('.grid-list').siblings('.overlay').removeClass('visible');
 		});
 	});
