35d5d78d6dae240d4a3b9b9eac56290260652ae2
bogdan.virtosu <virtosu.bogdan@gmail.com>
2015-10-30 18:43:46 +0200

    UI fixes.

--- cmsplugin_profile/static/admin/js/profile_admin.js
+++ cmsplugin_profile/static/admin/js/profile_admin.js
@@ -7,17 +7,22 @@
 		  	$('.order_field input').each(function(i){$(this).attr('value', i+1)});
 		  }
 		});
-		
+
 		$(document).on('click', '.profile-item-actions .edit-profile-item', function(e){
 			e.preventDefault();
 			$(this).parent('.profile-item-actions').siblings().addClass('visible');
 			$(this).closest('.grid-list').siblings('.overlay').addClass('visible');
 		});
 		$(document).on('click', '.grid-list .close-profile', function(e){
-			e.preventDefault();
+		    e.preventDefault();
 
-			$(this).closest('.visible').removeClass('visible');
-			$(this).closest('.grid-list').siblings('.overlay').removeClass('visible');
+		    $(this).closest('.visible').removeClass('visible');
+		    $(this).closest('.grid-list').siblings('.overlay').removeClass('visible');
+
+		    profile = $(this).closest('.ui-widget.inline-related');
+		    if(profile.attr('id') === "-") {
+			profile.remove();
+		    }
 		});
 	});
 })(jQuery);
