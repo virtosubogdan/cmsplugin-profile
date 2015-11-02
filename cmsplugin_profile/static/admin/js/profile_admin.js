49d86284c83621be91bf595b0456d9b21a445aa1
bogdan.virtosu <virtosu.bogdan@gmail.com>
2015-11-02 09:17:21 +0200

    Implement base for reverting profile changes.

--- cmsplugin_profile/static/admin/js/profile_admin.js
+++ cmsplugin_profile/static/admin/js/profile_admin.js
@@ -1,5 +1,6 @@
 (function($) {
 	$(document).ready(function() {
+	    previous_inputs = {};
 
 		$( ".grid-list" ).sortable({
 		  items: "> .inline-related",
@@ -11,7 +12,13 @@
 		$(document).on('click', '.profile-item-actions .edit-profile-item', function(e){
 			e.preventDefault();
 			$(this).parent('.profile-item-actions').siblings().addClass('visible');
-			$(this).closest('.grid-list').siblings('.overlay').addClass('visible');
+		        $(this).closest('.grid-list').siblings('.overlay').addClass('visible');
+
+		    previous_inputs = {};
+		    prefix = $(this)[0].attributes["data-profile-id"].value;
+		    $('input[name^="' + prefix + '"]').each(function(index, e) {
+			previous_inputs[$(this).attr("name")] = $(this)[0].value;
+		    });
 		});
 		$(document).on('click', '.grid-list .close-profile', function(e){
 		    e.preventDefault();
@@ -24,5 +31,17 @@
 			profile.remove();
 		    }
 		});
+	    $(document).on('click', '.grid-list .revert-profile-changes', function(e){
+		e.preventDefault();
+
+		$(this).closest('.visible').removeClass('visible');
+		$(this).closest('.grid-list').siblings('.overlay').removeClass('visible');
+
+		$.each(previous_inputs, function(key, value) {
+		    $('input[name="' + key + '"]')[0].value = value;
+		});
+
+	    });
+
 	});
 })(jQuery);
