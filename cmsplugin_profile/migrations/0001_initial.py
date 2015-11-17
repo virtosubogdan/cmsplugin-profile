f2721efd044d35c1eef236a219d83fcfc4a64416
bogdan.virtosu <virtosu.bogdan@gmail.com>
2015-11-17 09:35:35 +0200

    Regenerated migrations.

--- cmsplugin_profile/migrations/0001_initial.py
+++ cmsplugin_profile/migrations/0001_initial.py
@@ -18,22 +18,23 @@ class Migration(migrations.Migration):
             name='Profile',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('title', models.CharField(max_length=200, null=True, blank=True)),
+                ('title', models.CharField(max_length=60, null=True, blank=True)),
                 ('description', models.TextField(max_length=395)),
                 ('call_to_action_text', models.CharField(max_length=30, null=True, blank=True)),
                 ('call_to_action_url', models.CharField(max_length=200, null=True, blank=True)),
                 ('additional_links_label', models.CharField(default=b'', max_length=30, null=True, blank=True)),
                 ('image_credit', models.CharField(max_length=40, null=True, blank=True)),
-                ('detail_image', filer.fields.image.FilerImageField(related_name='profile_detail', on_delete=django.db.models.deletion.PROTECT, default=None, verbose_name='Detail Image', to='filer.Image', help_text='Image must be 1:1 aspect ratio')),
+                ('detail_image', filer.fields.image.FilerImageField(related_name='profile_detail', on_delete=django.db.models.deletion.PROTECT, default=None, verbose_name='Detail Image', to='filer.Image', help_text='Image must be 1:1 aspect ratio. We recommend 600px min size.')),
             ],
         ),
         migrations.CreateModel(
             name='ProfileGrid',
             fields=[
                 ('cmsplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin')),
-                ('title', models.CharField(max_length=200)),
+                ('title', models.CharField(max_length=60)),
                 ('description', models.TextField(default=b'', max_length=400, null=True, blank=True)),
                 ('show_title_on_thumbnails', models.BooleanField(default=False)),
+                ('load_mode_type', models.CharField(default=(b'load_mode_button', 'Load more button'), help_text='Button loading will load more profiles when the user clicks the button.Use this when the grid will be in a page with other elements. Scroll loaging will load more profiles when the user scrolls the page.Use this when the grid will be alone on the page.', max_length=20, verbose_name='Pagination type', choices=[(b'load_mode_button', 'Load more button'), (b'load_mode_scroll', 'Lazy loading')])),
             ],
             options={
                 'db_table': 'cmsplugin_profilegrid',
@@ -54,8 +55,8 @@ class Migration(migrations.Migration):
             name='ProfilePromoGrid',
             fields=[
                 ('cmsplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin')),
-                ('title', models.CharField(max_length=200, null=True, blank=True)),
-                ('call_to_action_text', models.CharField(max_length=200, null=True, blank=True)),
+                ('title', models.CharField(max_length=60, null=True, blank=True)),
+                ('call_to_action_text', models.CharField(max_length=100)),
                 ('profile_plugin', models.ForeignKey(to='cmsplugin_profile.ProfileGrid')),
             ],
             options={
@@ -84,7 +85,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='profile',
             name='thumbnail_image',
-            field=filer.fields.image.FilerImageField(related_name='profile_thumbnail', on_delete=django.db.models.deletion.PROTECT, default=None, verbose_name='Thumbnail Image', to='filer.Image', help_text='Image must be 1:1 aspect ratio'),
+            field=filer.fields.image.FilerImageField(related_name='profile_thumbnail', on_delete=django.db.models.deletion.PROTECT, default=None, verbose_name='Thumbnail Image', to='filer.Image', help_text='Image must be 1:1 aspect ratio. We recommend 440px min size.'),
         ),
         migrations.AlterOrderWithRespectTo(
             name='profile',
